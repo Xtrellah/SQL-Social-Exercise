545d1a5f9f839dde9fd3afb715e3413044cceff5
Max Estrella <maxestrella565@gmail.com>
2024-07-18 10:04:23 +0100

    Getting better

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $db = new PDO('mysql:host=db;dbname=social', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 
-$query = $db->prepare('SELECT `users`.`id` AS `user_id`, `users`.`username`, `posts`.`title`, `posts`.`image` 
+$query = $db->prepare('SELECT `users`.`id` AS `user_id`, `users`.`username`, `posts`.`title`, `posts`.`image`, `posts`.`content` 
                                 FROM `users` 
                                 LEFT JOIN `posts` 
                                 ON `users`.`id` = `posts`.`user_id`;');
@@ -13,11 +13,14 @@ $query = $db->prepare('SELECT `users`.`id` AS `user_id`, `users`.`username`, `po
 $query->execute();
 $users = $query->fetchAll();
 
-
+// POST
 echo '<ul>';
 foreach ($users as $user) {
-    echo "<li>{$user['username']} - {$user['id']}</li>";
-    echo "<li>{$user['title']} - {$user['user_id']}</li>";
-    echo "<img alt='image' src='{$user['image']}'/>";
+    echo "<div>
+                <h3>{$user['username']}</h3>
+                <h4>{$user['title']}</h4>
+                <img alt='image' src='{$user['image']}'/>
+                <li>{$user['content']}</li>
+          </div>";
 }
 echo '</ul>';
